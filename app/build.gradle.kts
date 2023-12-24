270de58eab59652262c1f6e7a0ede013fa9a999f
Lim <andrey.sidor.01@mail.ru>
2023-12-24 14:08:48 +0300

    Изменены зависимости gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.1.1")
 
     val okhttpVersion = "4.11.0"
     val retrofitVersion = "2.9.0"
